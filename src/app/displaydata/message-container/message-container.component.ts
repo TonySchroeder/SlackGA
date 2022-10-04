79e7d1a342c1faf3cd88862f129ba6cc10fe0416
Tony Schroeder <tony.schroeder@gmx.de>
2022-10-04 18:43:10 +0200

    add to edit message

--- src/app/displaydata/message-container/message-container.component.ts
+++ src/app/displaydata/message-container/message-container.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { doc, updateDoc } from 'firebase/firestore';
 import { delay, timeout } from 'rxjs';
 import { DialogDeleteMessageComponent } from 'src/app/dialog/dialog-delete-message/dialog-delete-message.component';
 import { FirebaseService } from '../../service/firebase.service';
@@ -15,10 +17,12 @@ export class MessageContainerComponent implements OnInit {
 
 
   public displayEditMenu;
-  messageToEdit:number;
+  messageToEdit: any;
 
   constructor(public store: FirebaseService,
-    public dialog: MatDialog) {
+    public dialog: MatDialog,
+    public firebase: Firestore
+  ) {
   }
 
   ngOnInit(): void {
@@ -26,11 +30,38 @@ export class MessageContainerComponent implements OnInit {
   }
 
 
-editMessage(index){
-  this.messageToEdit = index;
+  editMessage(index) {
+    this.messageToEdit = index;
+  }
 
-}
 
+  async saveEditMessage(currentMessageId: string) {
+    const loadMessage = doc(
+      this.firebase,
+      `messages/${currentMessageId}`
+    );
+    let editMessage = this.transform(this.store.messages, currentMessageId)
+    await updateDoc(loadMessage, { message: editMessage });
+    await this.store.loadMessages();
+    this.messageToEdit = '';
+  }
+
+
+
+  transform(value: any, filterString: any): any {
+    if (value && filterString) {
+      if (filterString.lenght === 0 || filterString === '') {
+        return undefined;
+      }
+      let currentMessages;
+      for (const message of value) {
+        if (message['currentMessageId'] === filterString) {
+          currentMessages = message.message
+        }
+      }
+      return currentMessages;
+    }
+  }
 
 
   openDialog(messageId): void {
