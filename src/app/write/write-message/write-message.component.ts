706ea9822dd53721171fcf645865ffbc23708aa3
JoannaKahloun <101954470+JoannaKahloun@users.noreply.github.com>
2022-10-15 18:35:42 +0200

    small cleaning again

--- src/app/write/write-message/write-message.component.ts
+++ src/app/write/write-message/write-message.component.ts
@@ -3,8 +3,6 @@ import { Firestore } from '@angular/fire/firestore';
 import { addDoc, doc, updateDoc } from 'firebase/firestore';
 import { Message } from 'src/app/models/message.class';
 import { FirebaseService } from '../../service/firebase.service';
-//import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill'
-// import 'quill-emoji/dist/quill-emoji.js'
 import { Output, EventEmitter } from '@angular/core';
 import Quill from 'quill'
 import BlotFormatter from 'quill-blot-formatter/dist/BlotFormatter';
@@ -17,8 +15,6 @@ Quill.register('modules/blotFormatter', BlotFormatter);
   styleUrls: ['./write-message.component.scss']
 })
 export class WriteMessageComponent implements OnInit {
-
-  //editor: Quill = new Quill('#editor');
 
   @Output() newItemEvent = new EventEmitter<string>();
   messageText: string = '';
@@ -44,33 +40,7 @@ export class WriteMessageComponent implements OnInit {
 }
 
   ngOnInit(): void {
-    /*
-    let toolbarOptions = [
-      ['bold', 'italic'],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      ['link', 'underline', 'blockquote', 'code-block'],
-      ['image', 'video'],
-    ];
-
-    // Quill configuration
-    let options = {
-      modules: {
-        blotFormatter: {
-          // empty object for default behaviour.
-        },
-        toolbar: toolbarOptions
-      },
-      placeholder: "Insert your message here...",
-      readOnly: false,
-      theme: 'snow'
-    };
-
-    // The quill instance
-    let editor = new Quill('#editor', options);
-
-    $(document).on('click', '#submit', function() {
-      $(this.message.messageText).html(editor.root.innerHTML);
-    });*/
+  
   }
 
   async saveAnswerInFirestore() {
